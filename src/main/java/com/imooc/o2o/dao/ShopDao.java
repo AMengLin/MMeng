66111a8901a73b7f4ab1296bcd4875f7a1d19e65
AMengLin <591185848@qq.com>
2019-01-24 17:45:06 +0800

    测试新添加商铺

---
+++ src/main/java/com/imooc/o2o/dao/ShopDao.java
@@ -0,0 +1,18 @@
+package com.imooc.o2o.dao;
+
+import com.imooc.o2o.entity.Shop;
+
+/**
+ * 店铺接口
+ * @author Lin
+ * @date 2019年1月24日 上午9:34:11
+ */
+public interface ShopDao {
+	
+	/**
+	 * 添加店铺
+	 * @param shop 客户新添加的店铺对象
+	 * @return
+	 */
+	int insertShop(Shop shop);
+}
--- src/main/java/com/imooc/o2o/web/AreaAction.java
+++ src/main/java/com/imooc/o2o/web/AreaAction.java
@@ -1,17 +1,41 @@
 package com.imooc.o2o.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.imooc.o2o.entity.Area;
 import com.imooc.o2o.service.AreaService;
 
 @Controller
+@RequestMapping("/areatest")
 public class AreaAction {
 	
 	@Autowired
 	private AreaService areaService;
 	
-	public void getList(){
-		areaService.getList();
+	@RequestMapping(value="/listarea",method=RequestMethod.GET)	
+	@ResponseBody
+	public Map<String,Object> listArea(){
+		Map<String,Object> map = new HashMap<String,Object>();
+		//查询返回区域的所有对象
+		List<Area> list = areaService.getList();
+		//把区域对象封装到map集合中返回给前端
+		try {
+			map.put("rows", list);
+			map.put("total", list.size());
+		} catch (Exception e) {
+			e.printStackTrace();
+			map.put("success", false);
+			map.put("errMsg", e.toString());
+		}
+		//返回最终的map集合
+		return map;
 	}
 }
