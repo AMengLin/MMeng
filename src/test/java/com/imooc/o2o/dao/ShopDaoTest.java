af6300000452c59a19399333e0e3656c6b1b0a8e
AMengLin <591185848@qq.com>
2019-05-27 09:43:38 +0800

    4

--- src/test/java/com/imooc/o2o/dao/ShopDaoTest.java
+++ src/test/java/com/imooc/o2o/dao/ShopDaoTest.java
@@ -20,7 +20,7 @@ public class ShopDaoTest extends BaseTest{
 	
 	@Test
 	public void insertShop(){
-		//创建商品对象13
+		//创建商品对象132
 		Shop shop = new Shop();
 		//创建个人信息对象
 		PersonInfo personInfo = new PersonInfo();
